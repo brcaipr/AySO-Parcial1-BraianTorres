c5f2ea958f47b3a6fda5fc703422ef966f09df66
braian torres <braiantorres228@gmail.com>
2024-10-18 01:58:43 +0000

    punto c modificacion

---
+++ puntoc.sh
@@ -0,0 +1,19 @@
+sudo useradd developer1
+sudo useradd tester1
+sudo useradd devops1
+sudo useradd devops2
+cat/etc/passwd
+cat /etc/passwd
+sudo groupadd grupodevops
+sudo groupadd grupodevelopers
+sudo groupadd grupotesters
+sudo groupadd grupodevops
+sudo usermod -aG grupodevelopers developer1
+sudo usermod -aG grupodevops developer1
+id developer1
+sudo usermod -aG grupodevops dev
+sudo usermod -aG grupodevops devops1
+id devops1
+sudo usermod -aG grupodevelopers devops1
+sudo usermod -aG grupodevops devops2
+id devops2
